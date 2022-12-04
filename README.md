7151aaa7ab98b03cff1c58c1061168d7227fa7ed
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-12-04 12:03:31 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# conversor-de-medidas
- 
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <div className="App">
+      <input type="number" />
+      <select name="from" id="from">
+
+      </select>
+    </div>
+  )
+}
+
+export default App
